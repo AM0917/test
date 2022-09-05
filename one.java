c30c465e61b7b9307c307a53ad45ce099d74616f
AM0917 <97365746+AM0917@users.noreply.github.com>
2022-09-05 15:19:27 -0500

    Create one.java

---
+++ one.java
@@ -0,0 +1,13 @@
+	public static void main(String[] args) 
+	{
+		int x = 0;
+		int y = 0;
+		Scanner num  = new Scanner(System.in);
+		System.out.println("Please input two numbers: ");
+		x = num.nextInt();
+		y = num.nextInt();
+		System.out.println(x , y);
+		
+		
+		
+	}
